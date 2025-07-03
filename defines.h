f38408bfcb9d3eae8d312b58c86df6cf57c5123e
MrCharizard306 <qitaoshi@gmail.com>
2025-07-03 21:31:05 +1000

    definitions and board repersentation

---
+++ defines.h
@@ -0,0 +1,27 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+typedef unsigned long long U64;
+
+#define NAME "Quokachi 1.0"
+#define BRD_SQ_NUM 120
+
+enum{ EMPTY, wPawn, wKnight, wBishop, wRook, wQueen, wKing,
+       bPawn, bKnight, bBishop, bRook, bQueen, bKing };
+enum{ fileA, fileB, fileC, fileD, fileE, fileF, fileG, fileH, fileNone};
+enum { rank1, rank2, rank3, rank4, rank5, rank6, rank7, rank8, rankNone};
+enum { WHITE, BLACK, BOTH };
+
+enum{
+    A1 = 21, B1, C1, D1, E1, F1, G1, H1,
+    A2 = 31, B2, C2, D2, E2, F2, G2, H2,
+    A3 = 41, B3, C3, D3, E3, F3, G3, H3,
+    A4 = 51, B4, C4, D4, E4, F4, G4, H4,
+    A5 = 61, B5, C5, D5, E5, F5, G5, H5,
+    A6 = 71, B6, C6, D6, E6, F6, G6, H6,
+    A7 = 81, B7, C7, D7, E7, F7, G7, H7,
+    A8 = 91, B8, C8, D8, E8, F8, G8, H8, NoSquare,
+};
+enum {FALSE, TRUE};
+
+#endif
---
+++ quokachi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "defines.h"
+
+int main() {
+    // Check
+    return 0;
+}
